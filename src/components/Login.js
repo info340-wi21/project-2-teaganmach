db3a0e4b14f934ae572d5bf73bf4dc2dd15e6a7e
znewton-1831937 <znewton@uw.edu>
2021-06-09 15:40:30 -0700

    added firebase authentication for user accounts

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../css/login.css";
-import {NavLink} from 'react-router-dom';
+import {NavLink, Redirect} from 'react-router-dom';
 
 export default function Login(){
     let [email, setEmail] = useState("");
     let [pass, setPass] = useState("");
+    let [successLogin, setSuccess] = useState(false);
     
     function checkValidity(){
         return email.length > 0 && pass.length > 0;
@@ -14,6 +17,18 @@ export default function Login(){
     
     function handleSubmit(event) {
         event.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(email, pass)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                setSuccess(true);
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setSuccess(false);
+            });
     }
     return (
     <div className="inner">
@@ -27,13 +42,16 @@ export default function Login(){
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" value={pass} onChange={(e) => setPass(e.target.value)}/>
             </Form.Group>
-            <Button block size="lg" type="submit" disabled={!checkValidity()}>
+            <Button block size="lg" type="submit" disabled={!checkValidity()} onClick={handleSubmit}>
                 Login
             </Button>
             <p className="text-right">
                 Don't have an account? <NavLink to="/register">Register here</NavLink>
             </p>
         </Form>
+        {successLogin &&
+          <Redirect to='/'/>
+        }     
     </div> 
     ); 
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import {NavLink} from 'react-router-dom';
+import {NavLink, Route, Redirect} from 'react-router-dom';
 import "../css/login.css";
 
 export default function Signup() {
@@ -10,6 +12,7 @@ export default function Signup() {
     let [email, setEmail] = useState("");
     let [pass, setPass] = useState("");
     let [passCheck, setPassCheck] = useState("");
+    let [successSignup, setSuccess] = useState(false);
         
     function checkValidity(){
         return email.length > 0 && pass.length > 0;
@@ -17,6 +20,17 @@ export default function Signup() {
         
     function handleSubmit(event) {
         event.preventDefault();
+        firebase.auth().createUserWithEmailAndPassword(email, pass)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                setSuccess(true);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setSuccess(false);
+            });
     }
     return (
         <div className="inner">
@@ -42,13 +56,16 @@ export default function Signup() {
                     <Form.Label>Re-enter password</Form.Label>
                     <Form.Control type="passCheck" value={passCheck} onChange={(e) => setPassCheck(e.target.value)}/>
                 </Form.Group>
-                <Button block size="lg" type="submit" disabled={!checkValidity()}>
+                <Button block size="lg" type="submit" disabled={!checkValidity()} onclick={handleSubmit}>
                     Sign up
                 </Button>
                 <p className="text-right">
                     Already have an account? <NavLink to="/login">Sign in here</NavLink>
                 </p>
             </Form>
+            {successSignup && 
+                <Redirect to='/'/>
+            }
         </div> 
         ); 
     }
