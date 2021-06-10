7b7c8278eba103812b91dfb76bbbad261e7b5413
teaganmach <teaganmach@gmail.com>
2021-06-10 01:09:23 -0500

    favslist

--- src/components/FavList.js
+++ src/components/FavList.js
@@ -1,40 +1,12 @@
 // this feature renders a list of activities that users can pick their favorites from!
 
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { FixedSizeList } from "react-window";
 import SAMPLE_FAVS from '../data/activities.json';
 import '../css/favorites.css';
-
-// making a comparison function
-function compareValues(key, order = 'asc') {
-    return function innerSort(a, b) {
-      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-        // property doesn't exist on either object
-        return 0;
-      }
-  
-      const varA = (typeof a[key] === 'string')
-        ? a[key].toUpperCase() : a[key];
-      const varB = (typeof b[key] === 'string')
-        ? b[key].toUpperCase() : b[key];
-  
-      let comparison = 0;
-      if (varA > varB) {
-        comparison = 1;
-      } else if (varA < varB) {
-        comparison = -1;
-      }
-      return (
-        (order === 'desc') ? (comparison * -1) : comparison
-      );
-    };
-}
-
-// sorting the list by name in alphabetical order -- not needed?
-let sorted_favs = SAMPLE_FAVS.sort(compareValues('name'))
 
 // making the overall theme of the page using material-ui
 // SUBJECT TO CHANGE
@@ -47,12 +19,35 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-// function that renders the the list 
-function renderRow(props) {
+export default function FavsList() {
+  // state var that keeps track of what's in the favs list
+  let [favs, setFavs] = useState([]);
+  let [act, setAct] = useState(SAMPLE_FAVS);
+  const classes = useStyles();
+
+  function addFavs(indexToAdd){
+    setFavs([...SAMPLE_FAVS.filter((_, index)=> index === indexToAdd)]);
+  }
+
+  // renders list of activities
+  function renderRow(props) {
     const { index, style } = props;
     // retrieve array of list item names
-    let result = SAMPLE_FAVS.map((obj) => obj.name).sort();
-  
+    let result = act.map((obj) => obj.name).sort();
+
+    return (
+      <ListItem button style={style} key={index} onClick={()=>addFavs(index)}>
+        <ListItemText primary={result[index]} />
+      </ListItem>
+    );
+  }
+
+  // renders list of favs
+  function renderFavs(props){
+  const { index, style } = props;
+    // retrieve array of list item names
+    let result = favs.sort().map((obj) => obj.name);
+
     return (
       <ListItem button style={style} key={index}>
         <ListItemText primary={result[index]} />
@@ -60,29 +55,33 @@ function renderRow(props) {
     );
   }
 
-export default function FavsList() {
-  const classes = useStyles();
-
-  return (
+	return (
     <div className="jumbotron">
       <div className="container">
         <div className="row">
-          <div className="left-panel col-sm">
+        <div className="favs-panel col-sm">
+          <h3>favorites go here!</h3>
+          <FixedSizeList
+            height={400}
+            width={300}
+            itemSize={40}
+            itemCount={act.length}>
+            {renderFavs}
+          </FixedSizeList>
+        </div>
+          <div className="left-panel col-sm-auto">
             <h3>Click on an activity to assign it as a favorite:</h3> 
-            <FixedSizeList
-              height={400}
-              width={300}
-              itemSize={40}
-              itemCount={SAMPLE_FAVS.length}>
-              {renderRow}
-            </FixedSizeList>
-          </div>
-          <div className="favs-panel col-sm-auto">
-            <h3>favorites go here!</h3>
-          </div>
+           <FixedSizeList
+            height={400}
+            width={300}
+            itemSize={40}
+            itemCount={SAMPLE_FAVS.length}>
+            {renderRow}
+          </FixedSizeList>
         </div>
       </div>
     </div>
-  );
+  </div>
+	);
 }
 
